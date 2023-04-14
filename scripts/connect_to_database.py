53f43a756b09454f4426e00cef35365ac93d9d23
linharesrocha <guilhermelinharesr@gmail.com>
2023-04-14 17:37:17 -0300

    removendo caminho absoluto

--- scripts/connect_to_database.py
+++ scripts/connect_to_database.py
@@ -4,8 +4,7 @@ from pathlib import Path
 import pyodbc
 
 def get_connection():
-    env_path = Path('.') / 'C:\workspace\cadastro-aton\mordomo\programas\.env-sql'
-    load_dotenv(dotenv_path=env_path)
+    load_dotenv()
     DATABASE = os.environ['DATABASE']
     UID = os.environ['UID']
     PWD = os.environ['PWD']
