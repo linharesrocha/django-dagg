5919410cfd826e2d91d1578d0003c96413edbe47
linharesrocha <guilhermelinharesr@gmail.com>
2023-04-17 11:00:26 -0300

    atualizando getenv

--- scripts/connect_to_database.py
+++ scripts/connect_to_database.py
@@ -3,14 +3,15 @@ import os
 
 def get_connection():
     load_dotenv()
-    DATABASE = os.environ['DATABASE']
-    UID = os.environ['UID']
-    PWD = os.environ['PWD']
+    DATABASE = os.getenv('DATABASE')
+    UID = os.getenv('UID')
+    PWD = os.getenv('PWD')
     connection = ("Driver={SQL Server};"
                     "Server=erp.ambarxcall.com.br;"
                     "Database=" + DATABASE + ";"
-                                            "UID=" + UID + ";"
-                                                            "PWD=" + PWD + ";")
+                    "UID=" + UID + ";"
+                    "PWD=" + PWD + ";"
+                    "TrustServerCertificate=yes;")
     return connection
 
 #
