26726f9ce7443f936e95bff54c7a57564225ad8f
linharesrocha <guilhermelinharesr@gmail.com>
2023-04-18 16:36:27 -0300

    Colocando nome

--- scripts/todas_vinculacoes_aton_marketplace.py
+++ scripts/todas_vinculacoes_aton_marketplace.py
@@ -11,13 +11,16 @@ def main():
     conexao = pyodbc.connect(connection)
 
     comando = f'''
-    SELECT B.CODID, B.COD_INTERNO, B.DESCRICAO, B.INATIVO, A.*
+    SELECT B.CODID, B.COD_INTERNO, B.DESCRICAO, C.ORIGEM_NOME, B.INATIVO, A.*
     FROM ECOM_SKU A
     LEFT JOIN MATERIAIS B ON A.MATERIAL_ID = B.CODID
+    LEFT JOIN ECOM_ORIGEM C ON A.ORIGEM_ID = C.ORIGEM_ID
     ORDER BY CODID
     '''
 
     df = pd.read_sql(comando, conexao)
+    
+    df.drop(columns=['MATERIAL_ID'], inplace=True)
     
     excel_bytes = BytesIO()
     df.to_excel(excel_bytes, index=False)
