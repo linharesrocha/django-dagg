53771ca310b83c279a8db0ddd089764908e441ae
linharesrocha <guilhermelinharesr@gmail.com>
2023-04-19 12:10:22 -0300

    criando view posicao

--- trackeamento/urls.py
+++ trackeamento/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('posicao-netshoes/', views.posicao_netshoes),
+    path('posicao-netshoes/cadastrar/', views.cadastrar_posicao_netshoes, name='cadastrar-posicao-netshoes'),
 ]
--- trackeamento/views.py
+++ trackeamento/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import PosicaoNetshoes
 
 def index(request):
     return render(request, 'trackeamento/index.html')
 
 def posicao_netshoes(request):
     return render(request, 'trackeamento/posicao-netshoes.html')
+
+def cadastrar_posicao_netshoes(request):
+    nova_posicao = PosicaoNetshoes()
+
+    termo = request.POST['termo']
+    sku_netshoes = request.POST['sku-netshoes']
+    
+    nova_posicao.termo_busca = termo
+    nova_posicao.sku_netshoes = sku_netshoes
+    
+    if termo == '' or sku_netshoes == '':
+        return HttpResponse('<script>alert("Preencha os campos vazios!"); window.history.back();</script>')
+        
+    
+    nova_posicao.save()
+    
+    return HttpResponse('<script>alert("Cadastro feito com sucesso!"); window.history.back();</script>')
