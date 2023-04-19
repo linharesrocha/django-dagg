220a9da84442c6b72c611377cfd05460a6d88be8
linharesrocha <guilhermelinharesr@gmail.com>
2023-04-19 11:20:10 -0300

    html posicao netshoes

--- trackeamento/urls.py
+++ trackeamento/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('posicao-netshoes/', views.posicao_netshoes),
 ]
--- trackeamento/views.py
+++ trackeamento/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'trackeamento/index.html')
+
+def posicao_netshoes(request):
+    return render(request, 'trackeamento/posicao-netshoes.html')
