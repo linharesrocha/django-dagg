95eff5bf912196bd3d65de85a3475046b952d454
linharesrocha <guilhermelinharesr@gmail.com>
2023-04-20 09:59:22 -0300

    Adicionando campo nome, pagina e anuncio_concorrente

--- trackeamento/models.py
+++ trackeamento/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 class PosicaoNetshoes(models.Model):
     id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=255)
     termo_busca = models.CharField(max_length=255)
     sku_netshoes = models.CharField(max_length=255)
     posicao = models.IntegerField(blank=True, null=True)
+    pagina = models.IntegerField(blank=True, null=True)
+    anuncio_concorrente = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
