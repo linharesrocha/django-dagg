61725298c5ac9d2d4ff12c7582fec3a8b8bd0e0a
linharesrocha <guilhermelinharesr@gmail.com>
2023-05-02 09:08:22 -0300

    Alterando basedir

--- dagg/settings.py
+++ dagg/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-            'read_default_file': 'my.cnf'
+            'read_default_file': str(BASE_DIR / 'my.cnf')
         }
     }
 }
