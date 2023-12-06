1c41e9bd66007f0fc46ddd3078e9f4f1568676e6
linharesrocha <guilhermelinharesr@gmail.com>
2023-12-06 10:55:13 -0300

    aaa

--- ferramentas/views.py
+++ ferramentas/views.py
@@ -798,131 +798,135 @@ def atualizar_info_fiscal_produtos(request):
         conexao = pyodbc.connect(connection)
         cursor = conexao.cursor()
 
+        if 'btn_consultar_info_fiscal' in request.POST:
+            pass
 
-        cod_interno = request.POST.get('cod_interno')
-        ncm = request.POST.get('ncm')
-        origem = request.POST.get('origem')
-        cest = request.POST.get('cest')
-        tabela_imposto = request.POST.get('tabela_imposto')
 
-        # Valida se existe cod_interno
-        try:
-            comando = f'''
-            SELECT COD_INTERNO
-            FROM MATERIAIS
-            WHERE COD_INTERNO = '{cod_interno}'
-            '''
+        if 'btn_alterar_info_fiscal' in request.POST:
+            cod_interno = request.POST.get('cod_interno')
+            ncm = request.POST.get('ncm')
+            origem = request.POST.get('origem')
+            cest = request.POST.get('cest')
+            tabela_imposto = request.POST.get('tabela_imposto')
 
-            df = pd.read_sql(comando, conexao)
-            if len(df) <= 0:
-                messages.add_message(request, constants.ERROR, 'COD_INTERNO não existe no Aton!')
+            # Valida se existe cod_interno
+            try:
+                comando = f'''
+                SELECT COD_INTERNO
+                FROM MATERIAIS
+                WHERE COD_INTERNO = '{cod_interno}'
+                '''
+
+                df = pd.read_sql(comando, conexao)
+                if len(df) <= 0:
+                    messages.add_message(request, constants.ERROR, 'COD_INTERNO não existe no Aton!')
+                    return redirect('index-ferramentas')
+            except:
+                messages.add_message(request, constants.ERROR, 'Erro no servidor!')
                 return redirect('index-ferramentas')
-        except:
-            messages.add_message(request, constants.ERROR, 'Erro no servidor!')
+            
+            # NCM
+            if str(ncm) == 'Apagar':
+                comando = f'''
+                    UPDATE MATERIAIS
+                    SET CLASS_FISCAL = NULL
+                    WHERE COD_INTERNO = '{cod_interno}'
+                    '''
+
+                cursor.execute(comando)
+                conexao.commit()
+            elif ncm != '':
+                if len(ncm) != 8:
+                    messages.add_message(request, constants.ERROR, 'NCM deve ter exatamente 8 digitos!')
+                    return redirect('index-ferramentas')
+                
+                try:
+                    # Atualiza NCM
+                    comando = f'''
+                    UPDATE MATERIAIS
+                    SET CLASS_FISCAL = '{ncm}'
+                    WHERE COD_INTERNO = '{cod_interno}'
+                    '''
+
+                    cursor.execute(comando)
+                    conexao.commit()
+                except:
+                    messages.add_message(request, constants.ERROR, 'Erro ao atualizar NCM!')
+                    return redirect('index-ferramentas')
+            
+            # ORIGEM
+            if str(origem) == 'Apagar':
+                comando = f'''
+                    UPDATE MATERIAIS
+                    SET ORIGEM_TRIB = NULL
+                    WHERE COD_INTERNO = '{cod_interno}'
+                    '''
+
+                cursor.execute(comando)
+                conexao.commit()
+            elif origem != '':
+                if len(origem) != 1:
+                    messages.add_message(request, constants.ERROR, 'Origem deve ter 1 digito!')
+                    return redirect('index-ferramentas')
+                
+                # Atualiza ORIGEM
+                try:
+                    comando = f'''
+                    UPDATE MATERIAIS
+                    SET ORIGEM_TRIB = '{origem}'
+                    WHERE COD_INTERNO = '{cod_interno}'
+                    '''
+
+                    cursor.execute(comando)
+                    conexao.commit()
+                except:
+                    messages.add_message(request, constants.ERROR, 'Erro ao atualizar ORIGEM!')
+                    return redirect('index-ferramentas')
+            
+            # CEST
+            if str(cest) == 'Apagar':
+                comando = f'''
+                    UPDATE MATERIAIS
+                    SET CEST = NULL
+                    WHERE COD_INTERNO = '{cod_interno}'
+                    '''
+
+                cursor.execute(comando)
+                conexao.commit()
+            elif cest != '':
+                if len(cest) != 7:
+                    messages.add_message(request, constants.ERROR, 'CEST deve ter 7 digitos!')
+                    return redirect('index-ferramentas')
+                
+                # Atualiza CEST
+                try:
+                    comando = f'''
+                    UPDATE MATERIAIS
+                    SET CEST = '{cest}'
+                    WHERE COD_INTERNO = '{cod_interno}'
+                    '''
+
+                    cursor.execute(comando)
+                    conexao.commit()
+                except:
+                    messages.add_message(request, constants.ERROR, 'Erro ao atualizar CEST!')
+                    return redirect('index-ferramentas')
+            
+            # Atualiza TABELA_IMPOSTO
+            if tabela_imposto != None:
+                try:
+                    comando = f'''
+                    UPDATE MATERIAIS
+                    SET TABELAIMPOSTO = '{tabela_imposto}'
+                    WHERE COD_INTERNO = '{cod_interno}'
+                    '''
+
+                    cursor.execute(comando)
+                    conexao.commit()
+                except:
+                    messages.add_message(request, constants.ERROR, 'Erro ao atualizar TABELA_IMPOSTO!')
+                    return redirect('index-ferramentas')
+
+            messages.add_message(request, constants.SUCCESS, 'Informações fiscais atualizadas com sucesso!')
             return redirect('index-ferramentas')
-        
-        # NCM
-        if str(ncm) == 'Apagar':
-            comando = f'''
-                UPDATE MATERIAIS
-                SET CLASS_FISCAL = NULL
-                WHERE COD_INTERNO = '{cod_interno}'
-                '''
-
-            cursor.execute(comando)
-            conexao.commit()
-        elif ncm != '':
-            if len(ncm) != 8:
-                messages.add_message(request, constants.ERROR, 'NCM deve ter exatamente 8 digitos!')
-                return redirect('index-ferramentas')
-            
-            try:
-                # Atualiza NCM
-                comando = f'''
-                UPDATE MATERIAIS
-                SET CLASS_FISCAL = '{ncm}'
-                WHERE COD_INTERNO = '{cod_interno}'
-                '''
-
-                cursor.execute(comando)
-                conexao.commit()
-            except:
-                messages.add_message(request, constants.ERROR, 'Erro ao atualizar NCM!')
-                return redirect('index-ferramentas')
-        
-        # ORIGEM
-        if str(origem) == 'Apagar':
-            comando = f'''
-                UPDATE MATERIAIS
-                SET ORIGEM_TRIB = NULL
-                WHERE COD_INTERNO = '{cod_interno}'
-                '''
-
-            cursor.execute(comando)
-            conexao.commit()
-        elif origem != '':
-            if len(origem) != 1:
-                messages.add_message(request, constants.ERROR, 'Origem deve ter 1 digito!')
-                return redirect('index-ferramentas')
-            
-            # Atualiza ORIGEM
-            try:
-                comando = f'''
-                UPDATE MATERIAIS
-                SET ORIGEM_TRIB = '{origem}'
-                WHERE COD_INTERNO = '{cod_interno}'
-                '''
-
-                cursor.execute(comando)
-                conexao.commit()
-            except:
-                messages.add_message(request, constants.ERROR, 'Erro ao atualizar ORIGEM!')
-                return redirect('index-ferramentas')
-        
-        # CEST
-        if str(cest) == 'Apagar':
-            comando = f'''
-                UPDATE MATERIAIS
-                SET CEST = NULL
-                WHERE COD_INTERNO = '{cod_interno}'
-                '''
-
-            cursor.execute(comando)
-            conexao.commit()
-        elif cest != '':
-            if len(cest) != 7:
-                messages.add_message(request, constants.ERROR, 'CEST deve ter 7 digitos!')
-                return redirect('index-ferramentas')
-            
-            # Atualiza CEST
-            try:
-                comando = f'''
-                UPDATE MATERIAIS
-                SET CEST = '{cest}'
-                WHERE COD_INTERNO = '{cod_interno}'
-                '''
-
-                cursor.execute(comando)
-                conexao.commit()
-            except:
-                messages.add_message(request, constants.ERROR, 'Erro ao atualizar CEST!')
-                return redirect('index-ferramentas')
-        
-        # Atualiza TABELA_IMPOSTO
-        if tabela_imposto != None:
-            try:
-                comando = f'''
-                UPDATE MATERIAIS
-                SET TABELAIMPOSTO = '{tabela_imposto}'
-                WHERE COD_INTERNO = '{cod_interno}'
-                '''
-
-                cursor.execute(comando)
-                conexao.commit()
-            except:
-                messages.add_message(request, constants.ERROR, 'Erro ao atualizar TABELA_IMPOSTO!')
-                return redirect('index-ferramentas')
-
-        messages.add_message(request, constants.SUCCESS, 'Informações fiscais atualizadas com sucesso!')
-        return redirect('index-ferramentas')
 
