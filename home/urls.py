644d77da92aa58c147b6f6d8b4b895c83760057e
linharesrocha <guilhermelinharesr@gmail.com>
2023-12-06 15:30:38 -0300

    aaaaa

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='index-home'),
+    path('atualizar_site/', views.atualizar_site, name='atualizar-site'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.messages import constants
+from django.contrib import messages
+import os
+import sys
+
 
 def index(request):
-    return render(request, 'home/index.html')
+    return render(request, 'index-home.html')
+
+
+def atualizar_site(request):
+    # faz git pull
+    os.system('git pull')
+    
+    messages.add_message(request, messages.SUCCESS, 'Site atualizado com sucesso!')
+    return redirect('index-home')
