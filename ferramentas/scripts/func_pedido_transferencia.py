1469a486322c680f676e8f3dfce24e98a7762628
linharesrocha <guilhermelinharesr@gmail.com>
2023-12-18 13:00:11 -0300

    aaaa

--- ferramentas/scripts/func_pedido_transferencia.py
+++ ferramentas/scripts/func_pedido_transferencia.py
@@ -1,14 +1,62 @@
 from scripts.connect_to_database import get_connection
 import pyodbc
+import pandas as pd
 
 def main(empresa_origem, empresa_destino, armazem_origem, armazem_destino, df_pedido_transferencia):
     
     connection = get_connection()
     conexao = pyodbc.connect(connection)
     cursor = conexao.cursor()
-    
+
+    # Strip CODI_INTERNO
+    df_pedido_transferencia['COD_INTERNO'] = df_pedido_transferencia['COD_INTERNO'].str.strip()
+
+    # Confere se o produto é KIT
+    for index, row in df_pedido_transferencia.iterrows():
+        cod_interno_kit = row['COD_INTERNO']
+        quant_kit = row['QUANT']
+        
+        comando = f'''
+        SELECT COD_INTERNO, DESMEMBRA
+        FROM MATERIAIS
+        WHERE COD_INTERNO = '{cod_interno_kit}'
+        AND DESMEMBRA = 'S'
+        '''
+
+        check_kit = pd.read_sql_query(comando, conexao)
+
+        if len(check_kit) > 0:
+            # remove o kit do df
+            df_pedido_transferencia = df_pedido_transferencia[df_pedido_transferencia['COD_INTERNO'] != cod_interno_kit]
+
+            comando = f'''
+            SELECT A.*, B.COD_INTERNO
+            FROM MATERIAIS_AGREGADOS A
+            LEFT JOIN MATERIAIS B ON A.CODID = B.CODID
+            WHERE B.COD_INTERNO = '{cod_interno_kit}'
+            '''
+            
+            df_kit = pd.read_sql_query(comando, conexao)
+
+            # Adiciona os produtos do kit no df
+            for index, row in df_kit.iterrows():
+                quant_agreg = row['QUANT']
+                quant_add = quant_agreg * quant_kit
+                codid_agreg = row['CODID_AGREG']
+
+                # Obtem o COD_INTERNO do produto agregado
+                comando = f'''
+                SELECT COD_INTERNO
+                FROM MATERIAIS
+                WHERE CODID = '{codid_agreg}'
+                '''
+                cod_interno_agreg = cursor.execute(comando).fetchone()[0]
+
+                df_temp = pd.DataFrame({'COD_INTERNO': [cod_interno_agreg], 'QUANT': [quant_add]})
+                df_pedido_transferencia = pd.concat([df_pedido_transferencia, df_temp], ignore_index=True)
+            
     # Consulta ultimo numero PEDIDO
-    comando = '''
+    comando = f'''
     SELECT TOP 1 PEDIDO
     FROM PEDIDO_TRANSFERENCIA
     ORDER BY PEDIDO DESC
