341bf5fe4181cb594867ce8eb814c84a35431711
linharesrocha <guilhermelinharesr@gmail.com>
2024-10-06 15:36:14 -0300

    aaa

--- scripts/confere_margem_netshoes.py
+++ scripts/confere_margem_netshoes.py
@@ -13,6 +13,8 @@ from openpyxl.styles import PatternFill
 import django
 from pathlib import Path
 import sys
+from datetime import datetime
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Set up Django settings
@@ -32,8 +34,6 @@ def encontra_nome_coluna(sheet, nome_coluna):
             return numero_coluna
         
 def cria_writer_and_send(df, empresa):
-
-
     # Salve em excel com a data de ontem
     name_file_excel = f'margem_netshoes_{empresa.strip().lower()}' + str(date.today() - timedelta(days=1)) + '.xlsx'
 
@@ -132,150 +132,169 @@ def cria_writer_and_send(df, empresa):
     except Exception as e:
         print(e)
 
-# Filtrando Warnings
-warnings.filterwarnings('ignore')
+def main(inicio_data_personalizada, fim_data_personalizada):
+    # Filtrando Warnings
+    warnings.filterwarnings('ignore')
 
-connection = get_connection()
-conexao = pyodbc.connect(connection)
-cursor = conexao.cursor()
+    connection = get_connection()
+    conexao = pyodbc.connect(connection)
+    cursor = conexao.cursor()
 
-# DAGG
-DAGG_COMISSAO_PADRAO = netshoes_tarifas.dagg_comissao_padrao
-DAGG_ACRESCIMO_COMISSAO = 0
-DAGG_DESCONTO_CAMPANHA = 0
+    # DAGG
+    DAGG_COMISSAO_PADRAO = netshoes_tarifas.dagg_comissao_padrao
+    DAGG_ACRESCIMO_COMISSAO = 0
+    DAGG_DESCONTO_CAMPANHA = 0
 
-# RED PLACE
-RED_COMISSAO_PADRAO = netshoes_tarifas.red_comissao_padrao
-RED_ACRESCIMO_COMISSAO = 0
-RED_DESCONTO_CAMPANHA = 0
+    # RED PLACE
+    RED_COMISSAO_PADRAO = netshoes_tarifas.red_comissao_padrao
+    RED_ACRESCIMO_COMISSAO = 0
+    RED_DESCONTO_CAMPANHA = 0
 
-# PISSTE
-PISSTE_COMISSAO_PADRAO = netshoes_tarifas.pisste_comissao_padrao
-PISSTE_ACRESCIMO_COMISSAO = 0
-PISSTE_DESCONTO_CAMPANHA = 0
+    # PISSTE
+    PISSTE_COMISSAO_PADRAO = netshoes_tarifas.pisste_comissao_padrao
+    PISSTE_ACRESCIMO_COMISSAO = 0
+    PISSTE_DESCONTO_CAMPANHA = 0
 
-# PADRAO
-TARIFA_FIXA = netshoes_tarifas.tarifa_fixa # PEDIDO ACIMA DE R$10,00
-OPERACAO = netshoes_tarifas.operacao
-IMPOSTO = netshoes_tarifas.imposto
+    # PADRAO
+    TARIFA_FIXA = netshoes_tarifas.tarifa_fixa # PEDIDO ACIMA DE R$10,00
+    OPERACAO = netshoes_tarifas.operacao
+    IMPOSTO = netshoes_tarifas.imposto
 
-comando = f'''
-SELECT DISTINCT A.CODID, C.MATERIAL_ID AS CODID_KIT, A.COD_INTERNO, D.COD_INTERNO AS COD_INTERNO_KIT, COD_PEDIDO AS SKU_MKTP, B.SEUPEDIDO AS PEDIDO, A.EMPRESA, A.DESCRICAOPROD AS TITULO, D.DESCRICAO AS TITULO_KIT, A.VLR_CUSTO, VLR_UNIT AS VLR_PEDIDO, B.VLRFRETE AS VLR_FRETE, DATA, D.DESMEMBRA AS KIT, A.QUANT
-FROM PEDIDO_MATERIAIS_ITENS_CLIENTE A
-LEFT JOIN PEDIDO_MATERIAIS_CLIENTE B ON A.PEDIDO = B.PEDIDO
-LEFT JOIN ECOM_SKU C ON A.COD_PEDIDO = C.SKU
-LEFT JOIN MATERIAIS D ON C.MATERIAL_ID = D.CODID
-WHERE B.TIPO = 'PEDIDO'
-AND B.POSICAO != 'CANCELADO'
-AND B.ORIGEM IN ('2', '3', '4')
-AND B.DATA >= DATEADD(DAY, DATEDIFF(DAY, 0, GETDATE()) - 6, 0)
-AND B.DATA < DATEADD(DAY, DATEDIFF(DAY, 0, GETDATE()), 0)
-ORDER BY A.EMPRESA, DATA
-'''
+    if inicio_data_personalizada == None and fim_data_personalizada == None:
+        comando = f'''
+        SELECT DISTINCT A.CODID, C.MATERIAL_ID AS CODID_KIT, A.COD_INTERNO, D.COD_INTERNO AS COD_INTERNO_KIT, COD_PEDIDO AS SKU_MKTP, B.SEUPEDIDO AS PEDIDO, A.EMPRESA, A.DESCRICAOPROD AS TITULO, D.DESCRICAO AS TITULO_KIT, A.VLR_CUSTO, VLR_UNIT AS VLR_PEDIDO, B.VLRFRETE AS VLR_FRETE, DATA, D.DESMEMBRA AS KIT, A.QUANT
+        FROM PEDIDO_MATERIAIS_ITENS_CLIENTE A
+        LEFT JOIN PEDIDO_MATERIAIS_CLIENTE B ON A.PEDIDO = B.PEDIDO
+        LEFT JOIN ECOM_SKU C ON A.COD_PEDIDO = C.SKU
+        LEFT JOIN MATERIAIS D ON C.MATERIAL_ID = D.CODID
+        WHERE B.TIPO = 'PEDIDO'
+        AND B.POSICAO != 'CANCELADO'
+        AND B.ORIGEM IN ('2', '3', '4')
+        AND B.DATA >= DATEADD(DAY, DATEDIFF(DAY, 0, GETDATE()) - 6, 0)
+        AND B.DATA < DATEADD(DAY, DATEDIFF(DAY, 0, GETDATE()), 0)
+        ORDER BY A.EMPRESA, DATA
+        '''
+    else:
+        data_inicial = datetime.datetime.strptime(inicio_data_personalizada, '%Y-%m-%d')
+        data_final = datetime.datetime.strptime(fim_data_personalizada, '%Y-%m-%d')
 
-df = pd.read_sql(comando, conexao)
+        comando = f'''
+        SELECT DISTINCT A.CODID, C.MATERIAL_ID AS CODID_KIT, A.COD_INTERNO, D.COD_INTERNO AS COD_INTERNO_KIT, COD_PEDIDO AS SKU_MKTP, B.SEUPEDIDO AS PEDIDO, A.EMPRESA, A.DESCRICAOPROD AS TITULO, D.DESCRICAO AS TITULO_KIT, A.VLR_CUSTO, VLR_UNIT AS VLR_PEDIDO, B.VLRFRETE AS VLR_FRETE, DATA, D.DESMEMBRA AS KIT, A.QUANT
+        FROM PEDIDO_MATERIAIS_ITENS_CLIENTE A
+        LEFT JOIN PEDIDO_MATERIAIS_CLIENTE B ON A.PEDIDO = B.PEDIDO
+        LEFT JOIN ECOM_SKU C ON A.COD_PEDIDO = C.SKU
+        LEFT JOIN MATERIAIS D ON C.MATERIAL_ID = D.CODID
+        WHERE B.TIPO = 'PEDIDO'
+        AND B.POSICAO != 'CANCELADO'
+        AND B.ORIGEM IN ('2', '3', '4')
+        AND B.DATA >= '{data_inicial.strftime('%Y-%m-%d')}'
+        AND B.DATA < '{(data_final + datetime.timedelta(days=1)).strftime('%Y-%m-%d')}'
+        ORDER BY A.EMPRESA, DATA
+        '''
 
-# Remove espaço da coluna TITULO
-df['TITULO'] = df['TITULO'].str.strip()
-df['TITULO_KIT'] = df['TITULO_KIT'].str.strip()
-df['COD_INTERNO'] = df['COD_INTERNO'].str.strip()
-df['COD_INTERNO_KIT'] = df['COD_INTERNO_KIT'].str.strip()
-df['PEDIDO'] = df['PEDIDO'].str.strip()
+    df = pd.read_sql(comando, conexao)
 
-is_kit_s = df['KIT'] == 'S'
+    # Remove espaço da coluna TITULO
+    df['TITULO'] = df['TITULO'].str.strip()
+    df['TITULO_KIT'] = df['TITULO_KIT'].str.strip()
+    df['COD_INTERNO'] = df['COD_INTERNO'].str.strip()
+    df['COD_INTERNO_KIT'] = df['COD_INTERNO_KIT'].str.strip()
+    df['PEDIDO'] = df['PEDIDO'].str.strip()
 
-# Altera CODID_KIT
-df['CODID'] = np.where(is_kit_s, df['CODID_KIT'], df['CODID'])
-df.drop('CODID_KIT', axis=1, inplace=True)
+    is_kit_s = df['KIT'] == 'S'
 
-# Altera COD_INTERNO_KIT
-df['COD_INTERNO'] = np.where(is_kit_s, df['COD_INTERNO_KIT'], df['COD_INTERNO'])
-df.drop('COD_INTERNO_KIT', axis=1, inplace=True)
+    # Altera CODID_KIT
+    df['CODID'] = np.where(is_kit_s, df['CODID_KIT'], df['CODID'])
+    df.drop('CODID_KIT', axis=1, inplace=True)
 
-# Altera TITULO_KIT
-df['TITULO'] = np.where(is_kit_s, df['TITULO_KIT'], df['TITULO'])
-df.drop('TITULO_KIT', axis=1, inplace=True)
+    # Altera COD_INTERNO_KIT
+    df['COD_INTERNO'] = np.where(is_kit_s, df['COD_INTERNO_KIT'], df['COD_INTERNO'])
+    df.drop('COD_INTERNO_KIT', axis=1, inplace=True)
 
-# Somando VLR_CUSTO e VLR_PEDIDO
-df['SOMA_VLR_CUSTO'] = 0
-df['SOMA_VLR_PEDIDO'] = 0
-mask_custo = (df['KIT'] == 'S')
-mask_pedido = (df['KIT'] == 'S')
-soma_custo = df[mask_custo].groupby('PEDIDO')['VLR_CUSTO'].transform(lambda x: (x * df.loc[x.index, 'QUANT']).sum())
-soma_pedido = df[mask_pedido].groupby('PEDIDO')['VLR_PEDIDO'].transform(lambda x: (x * df.loc[x.index, 'QUANT']).sum())
-df.loc[mask_custo, 'SOMA_VLR_CUSTO'] = soma_custo
-df.loc[mask_pedido, 'SOMA_VLR_PEDIDO'] = soma_pedido
-df['VLR_CUSTO'] = df['SOMA_VLR_CUSTO'].where(mask_custo, df['VLR_CUSTO'])
-df['VLR_PEDIDO'] = df['SOMA_VLR_PEDIDO'].where(mask_pedido, df['VLR_PEDIDO'])
-df.drop(columns=['SOMA_VLR_CUSTO', 'SOMA_VLR_PEDIDO'], inplace=True)
+    # Altera TITULO_KIT
+    df['TITULO'] = np.where(is_kit_s, df['TITULO_KIT'], df['TITULO'])
+    df.drop('TITULO_KIT', axis=1, inplace=True)
 
-# Removendo duplicadas
-df_s = df[df['KIT'] == 'S']
-df_s_no_duplicates = df_s.drop_duplicates()
-df = pd.concat([df[df['KIT'] != 'S'], df_s_no_duplicates])
+    # Somando VLR_CUSTO e VLR_PEDIDO
+    df['SOMA_VLR_CUSTO'] = 0
+    df['SOMA_VLR_PEDIDO'] = 0
+    mask_custo = (df['KIT'] == 'S')
+    mask_pedido = (df['KIT'] == 'S')
+    soma_custo = df[mask_custo].groupby('PEDIDO')['VLR_CUSTO'].transform(lambda x: (x * df.loc[x.index, 'QUANT']).sum())
+    soma_pedido = df[mask_pedido].groupby('PEDIDO')['VLR_PEDIDO'].transform(lambda x: (x * df.loc[x.index, 'QUANT']).sum())
+    df.loc[mask_custo, 'SOMA_VLR_CUSTO'] = soma_custo
+    df.loc[mask_pedido, 'SOMA_VLR_PEDIDO'] = soma_pedido
+    df['VLR_CUSTO'] = df['SOMA_VLR_CUSTO'].where(mask_custo, df['VLR_CUSTO'])
+    df['VLR_PEDIDO'] = df['SOMA_VLR_PEDIDO'].where(mask_pedido, df['VLR_PEDIDO'])
+    df.drop(columns=['SOMA_VLR_CUSTO', 'SOMA_VLR_PEDIDO'], inplace=True)
 
-# Quantidade Pedidos
-df['QUANTIDADE_PED'] = df.groupby(['EMPRESA', 'PEDIDO'])['PEDIDO'].transform('count')
+    # Removendo duplicadas
+    df_s = df[df['KIT'] == 'S']
+    df_s_no_duplicates = df_s.drop_duplicates()
+    df = pd.concat([df[df['KIT'] != 'S'], df_s_no_duplicates])
 
-# Arrendonda coluna VLR_PEDIDO para 2 casas decimais
-df['VLR_PEDIDO'] = round(df['VLR_PEDIDO'], 2)
+    # Quantidade Pedidos
+    df['QUANTIDADE_PED'] = df.groupby(['EMPRESA', 'PEDIDO'])['PEDIDO'].transform('count')
 
-# Adiciona coluna VLR_TOTAL
-df['VLR_TOTAL'] = df['VLR_PEDIDO'] + df['VLR_FRETE']
+    # Arrendonda coluna VLR_PEDIDO para 2 casas decimais
+    df['VLR_PEDIDO'] = round(df['VLR_PEDIDO'], 2)
 
-# Coloca a coluna VLR_TOTAL depois do VLR_FRETE
-df = df[['CODID', 'COD_INTERNO', 'PEDIDO', 'EMPRESA', 'TITULO', 'VLR_CUSTO', 'VLR_PEDIDO', 'VLR_FRETE', 'VLR_TOTAL', 'DATA', 'KIT','QUANTIDADE_PED']]
+    # Adiciona coluna VLR_TOTAL
+    df['VLR_TOTAL'] = df['VLR_PEDIDO'] + df['VLR_FRETE']
 
-# Modifica VLR_FRETE
-df['VLR_FRETE'] = (df['VLR_FRETE'] / df['QUANTIDADE_PED']).round(2)
+    # Coloca a coluna VLR_TOTAL depois do VLR_FRETE
+    df = df[['CODID', 'COD_INTERNO', 'PEDIDO', 'EMPRESA', 'TITULO', 'VLR_CUSTO', 'VLR_PEDIDO', 'VLR_FRETE', 'VLR_TOTAL', 'DATA', 'KIT','QUANTIDADE_PED']]
 
-# Adiciona coluna TARIFA FIXA para apenas os valores da coluna VLR_PEDIDO acima de R$10,00 se não 0
-df['TARIFA_FIXA'] = df['VLR_PEDIDO'].apply(lambda x: TARIFA_FIXA if x > 10 else 0)
+    # Modifica VLR_FRETE
+    df['VLR_FRETE'] = (df['VLR_FRETE'] / df['QUANTIDADE_PED']).round(2)
 
-# Modifica TARIFICA_FIXA
-df['TARIFA_FIXA'] = (df['TARIFA_FIXA'] / df['QUANTIDADE_PED']).round(2)
+    # Adiciona coluna TARIFA FIXA para apenas os valores da coluna VLR_PEDIDO acima de R$10,00 se não 0
+    df['TARIFA_FIXA'] = df['VLR_PEDIDO'].apply(lambda x: TARIFA_FIXA if x > 10 else 0)
 
-# Imposto Frete que sigifnica aba IMPOSSTO * VALOR DO FRETE
-df['IMPOSTO_FRETE'] = (df['VLR_FRETE'] * (IMPOSTO / 100)).round(2)
+    # Modifica TARIFICA_FIXA
+    df['TARIFA_FIXA'] = (df['TARIFA_FIXA'] / df['QUANTIDADE_PED']).round(2)
 
-# Adiciona coluna OPERACAO
-df['OPERACAO'] = OPERACAO
+    # Imposto Frete que sigifnica aba IMPOSSTO * VALOR DO FRETE
+    df['IMPOSTO_FRETE'] = (df['VLR_FRETE'] * (IMPOSTO / 100)).round(2)
 
-# Adiciona coluna IMPOSTO
-df['IMPOSTO'] = IMPOSTO
+    # Adiciona coluna OPERACAO
+    df['OPERACAO'] = OPERACAO
 
-# Adiciona coluna COMISSAO_PADRAO mas diferenciando por empresa
-df['COMISSAO_PADRAO'] = df['EMPRESA'].apply(lambda x: DAGG_COMISSAO_PADRAO if x == 1 else RED_COMISSAO_PADRAO if x == 2 else PISSTE_COMISSAO_PADRAO)
+    # Adiciona coluna IMPOSTO
+    df['IMPOSTO'] = IMPOSTO
 
-# Adiciona coluna ACRESCIMO_COMISSAO mas diferenciando por empresa
-df['ACRESCIMO_COMISSAO'] = df['EMPRESA'].apply(lambda x: DAGG_ACRESCIMO_COMISSAO if x == 1 else RED_ACRESCIMO_COMISSAO if x == 2 else PISSTE_ACRESCIMO_COMISSAO)
+    # Adiciona coluna COMISSAO_PADRAO mas diferenciando por empresa
+    df['COMISSAO_PADRAO'] = df['EMPRESA'].apply(lambda x: DAGG_COMISSAO_PADRAO if x == 1 else RED_COMISSAO_PADRAO if x == 2 else PISSTE_COMISSAO_PADRAO)
 
-# Adiciona coluna DESCONTO_CAMPANHA mas diferenciando por empresa
-df['DESCONTO_CAMPANHA'] = df['EMPRESA'].apply(lambda x: DAGG_DESCONTO_CAMPANHA if x == 1 else RED_DESCONTO_CAMPANHA if x == 2 else PISSTE_DESCONTO_CAMPANHA)
+    # Adiciona coluna ACRESCIMO_COMISSAO mas diferenciando por empresa
+    df['ACRESCIMO_COMISSAO'] = df['EMPRESA'].apply(lambda x: DAGG_ACRESCIMO_COMISSAO if x == 1 else RED_ACRESCIMO_COMISSAO if x == 2 else PISSTE_ACRESCIMO_COMISSAO)
 
-# Adiciona coluna PORC_TOTAL que soma as colunas
-df['CUSTO_PARCIAL%'] = df['OPERACAO'] + df['IMPOSTO'] + df['COMISSAO_PADRAO'] + df['ACRESCIMO_COMISSAO'] - df['DESCONTO_CAMPANHA']
+    # Adiciona coluna DESCONTO_CAMPANHA mas diferenciando por empresa
+    df['DESCONTO_CAMPANHA'] = df['EMPRESA'].apply(lambda x: DAGG_DESCONTO_CAMPANHA if x == 1 else RED_DESCONTO_CAMPANHA if x == 2 else PISSTE_DESCONTO_CAMPANHA)
 
-# Calculo resto
-df['CUSTO_PARCIAL$'] = round(df['VLR_PEDIDO'] * (df['CUSTO_PARCIAL%'] / 100), 2)
+    # Adiciona coluna PORC_TOTAL que soma as colunas
+    df['CUSTO_PARCIAL%'] = df['OPERACAO'] + df['IMPOSTO'] + df['COMISSAO_PADRAO'] + df['ACRESCIMO_COMISSAO'] - df['DESCONTO_CAMPANHA']
 
-# Lucro
-df['LUCRO'] = round(df['VLR_PEDIDO'] - df['CUSTO_PARCIAL$'] - df['TARIFA_FIXA'] - df['VLR_CUSTO'] - df['IMPOSTO_FRETE'], 2)
+    # Calculo resto
+    df['CUSTO_PARCIAL$'] = round(df['VLR_PEDIDO'] * (df['CUSTO_PARCIAL%'] / 100), 2)
 
-# Margem
-df['MARGEM'] = round(df['LUCRO'] / df['VLR_PEDIDO'] * 100, 2)
+    # Lucro
+    df['LUCRO'] = round(df['VLR_PEDIDO'] - df['CUSTO_PARCIAL$'] - df['TARIFA_FIXA'] - df['VLR_CUSTO'] - df['IMPOSTO_FRETE'], 2)
 
-# Altera coluna EMPRESA para nomes
-df['EMPRESA'] = df['EMPRESA'].replace(1, 'DAGG')
-df['EMPRESA'] = df['EMPRESA'].replace(2, 'RED PLACE')
-df['EMPRESA'] = df['EMPRESA'].replace(3, 'PISSTE')
+    # Margem
+    df['MARGEM'] = round(df['LUCRO'] / df['VLR_PEDIDO'] * 100, 2)
 
-# Create 3 dataframes by EMPRESA
-df_dagg = df[df['EMPRESA'] == 'DAGG']
-df_red = df[df['EMPRESA'] == 'RED PLACE']
-df_pisste = df[df['EMPRESA'] == 'PISSTE']
+    # Altera coluna EMPRESA para nomes
+    df['EMPRESA'] = df['EMPRESA'].replace(1, 'DAGG')
+    df['EMPRESA'] = df['EMPRESA'].replace(2, 'RED PLACE')
+    df['EMPRESA'] = df['EMPRESA'].replace(3, 'PISSTE')
 
-# Passa os 3 dataframas para função writer
-cria_writer_and_send(df_dagg, 'DAGG')
-cria_writer_and_send(df_red, 'RED PLACE')
-cria_writer_and_send(df_pisste, 'PISSTE')
+    # Create 3 dataframes by EMPRESA
+    df_dagg = df[df['EMPRESA'] == 'DAGG']
+    df_red = df[df['EMPRESA'] == 'RED PLACE']
+    df_pisste = df[df['EMPRESA'] == 'PISSTE']
+
+    # Passa os 3 dataframas para função writer
+    cria_writer_and_send(df_dagg, 'DAGG')
+    cria_writer_and_send(df_red, 'RED PLACE')
+    cria_writer_and_send(df_pisste, 'PISSTE')
---
+++ scripts/start_confere_margem_netshoes.py
@@ -0,0 +1,6 @@
+from confere_margem_netshoes import main
+
+inicio_data_personalizada = None
+fim_data_personalizada = None
+
+main(inicio_data_personalizada, fim_data_personalizada)
